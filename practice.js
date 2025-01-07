5fa02dfb2f66e1c8828078808950d8366cf52a72
amod <119671543+aamodmishra@users.noreply.github.com>
2025-01-07 16:57:46 +0545

    hi

--- practice.js
+++ practice.js
@@ -2,12 +2,33 @@ let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "
 
 let stageEl= document.getElementById("stage")
 let fightButton= document.getElementById("fightButton")
+let announcement=document.getElementById("announcement")
+let count =0;
 
-fightButton.addEventListener("click",function(){
-    let randomIndexOne = Math.floor( Math.random() * fighters.length )
-    let randomIndexTwo = Math.floor( Math.random() * fighters.length )
-    stageEl.textContent=fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
+function onclick(){
+let randomIndexOne = Math.floor( Math.random() * fighters.length )
+let randomIndexTwo = Math.floor( Math.random() * fighters.length )
+stageEl.textContent=fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
+if(randomIndexOne === randomIndexTwo){
+    announcement.textContent="Finally you won 🥳🎉";
+    
+}else{ 
+    
+    announcement.textContent="try again🥺";
 }
 
 
-)
+// fightButton.addEventListener('click', () => {
+//     count++; // Increment the counter
+//     if (count++===3){
+//         announcement.textContent="try again🥺";
+//     }
+//     else{
+//         announcement.textContent="";
+//     }
+
+//   })
+}
+
+fightButton.addEventListener("click",onclick)
+
