db76da689bb579dea2c699c9f04cf19887906aa8
amod <119671543+aamodmishra@users.noreply.github.com>
2025-01-08 14:23:24 +0545

    hi

--- practice.js
+++ practice.js
@@ -4,30 +4,18 @@ let stageEl= document.getElementById("stage")
 let fightButton= document.getElementById("fightButton")
 let announcement=document.getElementById("announcement")
 let count =0;
-
-function onclick(){
-let randomIndexOne = Math.floor( Math.random() * fighters.length )
-let randomIndexTwo = Math.floor( Math.random() * fighters.length )
-stageEl.textContent=fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
-
-if(randomIndexOne === randomIndexTwo){
-    announcement.textContent="Finally you won 🥳🎉";
-    
-}else{ 
-    
-    announcement.textContent="try again🥺";
-}
-}
 fightButton.addEventListener("click",onclick)
-// we can apply this to make it more interesting .
-// fightButton.addEventListener('click', () => {
-//     count++; // Increment the counter
-//     if (count++===3){
-//         announcement.textContent="try again🥺";
-//     }
-//     else{
-//         announcement.textContent="";
-//     }
-//   })
-// fightButton.addEventListener("click",onclick)
+function onclick(){
+    let randomIndexOne = Math.floor( Math.random() * fighters.length )
+    let randomIndexTwo = Math.floor( Math.random() * fighters.length )
+    stageEl.textContent=fighters[randomIndexOne] + " vs " + fighters[randomIndexTwo]
+
+    if(randomIndexOne === randomIndexTwo){
+        announcement.textContent="Finally you won 🥳🎉"
+        
+    }else{ 
+        
+        announcement.textContent="try again🥺"
+    }
+}
 
